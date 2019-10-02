bd961e3d3942ff61a1d4aec2bd7bf8175491bdeb
Ignatios Drakoulas <ignatisnb@gmail.com>
2019-10-02 12:12:29 +0300

    Update tcp.sh

--- tcp.sh
+++ tcp.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
 function usage() {
+    echo -e "This utility uses the '/dev/tcp' which is a system file that allows you to interact directly with the tcp protocol.\r\n"
     echo "Usage:"
-    echo "$0 [-m METHOD] [-b BODY] [-H HEADER] [-hD]"
-    echo -e  "-m METHOD \tThe http method to use (default GET)"
-    echo -e  "-b BODY \tThe request body"
-    echo -e  "-H HEADER \tAn HTTP header (can include multiple)"
-    echo -e  "-h \t\tPrints this message and exits"
-    echo -e  "-D \t\tEnable debug to see get a look at the recorded inputs"
+    echo "  $(basename $0) [-m METHOD] [-b BODY] [-H HEADER] [-hD] URL"
+    echo -e  "    -m METHOD \tThe http method to use (default GET)"
+    echo -e  "    -b BODY \tThe request body"
+    echo -e  "    -H HEADER \tAn HTTP header (can include multiple)"
+    echo -e  "    -h \t\tPrints this message and exits"
+    echo -e  "    -D \t\tEnable debug to see get a look at the recorded inputs"
+    echo -e  "    URL \t\tThe url of the request (Required)"
+    echo -e  "\r\nExample:"
+    echo -e  "  $(basename $0) -m POST -H 'Content-Type: application/json' \\ \r\n  -b '{\"email\":\"user@example.com\",\"password\":\"password\"}' \\ \r\n  http:/example.com:3000/auth/login"
     exit
 }
 HOST=127.0.0.1
@@ -38,11 +42,17 @@ H)
 esac
 done
 shift $(( OPTIND - 1 ))
+if [ -z "${1}" ]; then
+    usage
+fi
 URL="${1#https://}"
 URL="${URL#http://}"
 HOST="${URL%%/*}"
 URL="/${URL#*/}"
-PORT="${HOST##*:}"
+TPORT="${HOST##*:}"
+if [ -z "${TPORT}" ]; then
+    PORT="${TPORT}"
+fi
 HOST="${HOST%%:*}"
 REQUEST="${METHOD} ${URL} HTTP/1.0\r\n"
 for HEADER in "${HEADERS[@]}"; do
@@ -58,11 +68,8 @@ else
 fi
 
 if [ "${DEBUG}" = "on" ]; then
-    echo "Method: $METHOD"
     echo "Host: $HOST"
     echo "Port: $PORT"
-    echo "Uri: $URL"
-    echo "Body: $BODY"
     echo "-----------------------------"
     echo "- Request:                  -"
     echo "-----------------------------"
